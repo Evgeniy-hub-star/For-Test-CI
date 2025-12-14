ffd63e600f14857fb46ad1b645ae20c9c534f453
Evgeniy <johny1102@mail.ru>
2025-12-15 01:25:48 +0300

    Debug test

--- test_ex.py
+++ test_ex.py
@@ -2,6 +2,7 @@ import unittest
 import sys
 import os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+from app import app
 import app as tested_app
 
 class FlaskAppTests(unittest.TestCase):
