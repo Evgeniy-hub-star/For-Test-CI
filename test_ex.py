516acd82da4007fb47ba4a3a33a8d2e64d63f175
Evgeniy <johny1102@mail.ru>
2025-12-15 13:00:54 +0300

    Added full test

--- test_ex.py
+++ test_ex.py
@@ -13,9 +13,25 @@ class FlaskAppTests(unittest.TestCase):
         self.app.testing = True
 
     def test_success_add(self):
-        r = self.app.get('/calc?a=9&b=5&op=add')
+        r = self.app.get('/calc?a=1&b=4&op=add')
         self.assertEqual(r.status_code, 200)
         self.assertEqual(r.data, b'5.0')
+
+    def test_success_sub(self):
+        r = self.app.get('/calc?a=9&b=4&op=sub')
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.data, b'5.0')
+
+    def test_success_mul(self):
+        r = self.app.get('/calc?a=1&b=5&op=mul')
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.data, b'5.0')
+
+    def test_success_div(self):
+        r = self.app.get('/calc?a=5&b=1&op=div')
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.data, b'5.0')
+
 
 if __name__=='__main__':
     unittest.main()
